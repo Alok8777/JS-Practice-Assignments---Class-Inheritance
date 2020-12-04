b9395aa840080c1ee3fd9abb783f833427d77b25
Alok8777 <68750984+Alok8777@users.noreply.github.com>
2020-12-04 10:17:43 +0530

    Changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,7 @@
-import React, { Component } from "react";
+import React, { Component, useState } from "react";
 import "../styles/App.css";
-import Circle from "./Circle";
-import Shape from "./Shape";
+import Circle from "./circle";
+import Shape from "./shape";
 class App extends Component {
   render() {
     return (
