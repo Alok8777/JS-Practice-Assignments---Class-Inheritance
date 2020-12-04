80bddf347858c0842a65c8fed31b13de9d478a2b
Alok8777 <68750984+Alok8777@users.noreply.github.com>
2020-12-04 10:10:13 +0530

    changes

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import "../styles/App.css";
+import Circle from "./Circle";
+import Shape from "./Shape";
+class App extends Component {
+  render() {
+    return (
+      <>
+        <Circle />
+        <Shape />
+      </>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/Circle.js
@@ -0,0 +1,12 @@
+// import using require
+let Shape = require("./Shape");
+// declare class
+class Circle extends Shape {
+  constructor() {
+    super();
+    this.color = "blue";
+  }
+  calculateArea() {}
+}
+module.exports = Circle;
+// export class using module.exports
